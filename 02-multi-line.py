0825595fd0234262915f4b1f2d844a37d27dedc7
Harsha <harsha.pakanathi@gmail.com>
2025-01-29 21:36:46 +0530

    Python

---
+++ 02-multi-line.py
@@ -0,0 +1,6 @@
+print(""" 
+      hello 
+      user
+      i am from
+      python
+      """)
---
+++ 04-data-type.py
@@ -0,0 +1,5 @@
+print(10/5)
+print(10//5)
+print('a' + 'b')
+print("data" + "type")
+print("data" + " " + "type")
