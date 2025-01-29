55d2f782bf33c9ae876e16f1c53e96de9f798601
Harsha <harsha.pakanathi@gmail.com>
2025-01-29 21:47:48 +0530

    python

--- 04-data-type.py
+++ 04-data-type.py
@@ -1,5 +1,17 @@
+order="chilli"
+place=" hyd"
+a=10
+b=7
+c=a+b
+print(c)
+print(order + place)
 print(10/5)
 print(10//5)
 print('a' + 'b')
 print("data" + "type")
 print("data" + " " + "type")
+
+pi=3.14
+radius=5
+area=pi*radius**2
+print(area)
